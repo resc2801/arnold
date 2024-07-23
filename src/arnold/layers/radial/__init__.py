618242c0523671f163fc7a7f9e27ffad6d7b26bc
rmrschub <rene.schubotz@dfki.de>
2024-07-23 11:36:58 +0200

    forgot to add...

--- src/arnold/layers/radial/__init__.py
+++ src/arnold/layers/radial/__init__.py
@@ -2,9 +2,9 @@
 Implementation of KAN layers using radial basis functions.
 """
 
-from .gaussian_rbf import GaussianRBF
-from .inverse_quadratic_rbf import InverseQuadraticRBF
-from .inverse_multiquadric_rbf import InverseMultiQuadricRBF
+from .gaussian import GaussianRBF
+from .inverse_quadratic import InverseQuadraticRBF
+from .inverse_multiquadric import InverseMultiQuadricRBF
 
 __all__ = [
     "GaussianRBF", 
