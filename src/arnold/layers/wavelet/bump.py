4fcd6a2d6fcb4a676a4b368c72905e5c5b6c3ab9
rmrschub <rene.schubotz@dfki.de>
2024-07-23 13:12:27 +0200

    bugfix in Bump wavelet

--- src/arnold/layers/wavelet/bump.py
+++ src/arnold/layers/wavelet/bump.py
@@ -9,4 +9,7 @@ class Bump(WaveletBase):
 
     @tf.function        
     def get_wavelets(self, x):
-        return tf.exp(-1.0 / (1 - x**2))
+        # Bump wavelet is only defined in the interval (-1, 1)
+        # We apply a condition to restrict the computation to this interval
+        inside_interval = (x > -1.0) & (x < 1.0)
+        return tf.exp(-1.0 / (1 - x**2)) * inside_interval.float()
