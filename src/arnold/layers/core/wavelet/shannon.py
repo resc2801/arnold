c973d26ee443f3316ddc6b0362ebed421d03b27e
rmrschub <rene.schubotz@dfki.de>
2024-07-29 15:22:23 +0200

    sinc(x) implementation working, but somewhat slow

--- src/arnold/layers/core/wavelet/shannon.py
+++ src/arnold/layers/core/wavelet/shannon.py
@@ -7,16 +7,16 @@ tfkl = tfk.layers
 
 
 class Shannon(WaveletBase):
-    """
-    NOTE: Defunct
-    """
 
     @tf.function
     def get_wavelets(self, x):
-        sinc = tf.experimental.numpy.sinc(x / np.pi) 
+        # See: https://mathworld.wolfram.com/SincFunction.html
+        indices = tf.where(tf.equal(x, 0))
+        x = tf.math.divide_no_nan(tf.math.sin(x), x)
+        sinc_x = tf.tensor_scatter_nd_update(x, indices, tf.ones((tf.shape(indices)[0], )))
         
         # Applying a Hamming window to limit the infinite support of the sinc function
         window = tf.signal.hamming_window(tf.shape(x)[-1], periodic=False, dtype=x.dtype)
         
         # Shannon wavelet is the product of the sinc function and the window
-        return (sinc * window)
+        return (sinc_x * window)
